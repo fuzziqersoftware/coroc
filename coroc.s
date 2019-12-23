c8b55e2e27e1895c6cae63d943a1b174ab211f6f
Martin Michelsen <mjem@wildblue.net>
2019-12-23 11:40:05 -0800

    reformat the assembly code and stop using numbered labels

--- coroc.s
+++ coroc.s
@@ -3,7 +3,7 @@
 
 .globl _coro_start
 _coro_start:
-  mov rsi, 0x4000
+  mov   rsi, 0x4000
 
 .globl _coro_start_stack
 _coro_start_stack:
@@ -27,92 +27,92 @@ _coro_start_stack:
   #   - addr of coro_return
   #   - coroutine object 
 
-  push rsi
+  push    rsi
 
   # allocate a new coroutine stack. the coroutine struct will be at the very top
   # of the stack, and the stack will grow downward beyond it. note that rsi
   # contains the stack size already
-  mov rdi, 0  # addr
-  mov rdx, 0x03  # prot = PROT_READ|PROT_WRITE
-  mov rcx, 0x1002  # flags = MAP_ANONYMOUS|MAP_PRIVATE
-  mov r8, -1  # fd
-  mov r9, 0  # offset
-  call _mmap
-  pop rsi
+  mov     rdi, 0  # addr
+  mov     rdx, 0x03  # prot = PROT_READ|PROT_WRITE
+  mov     rcx, 0x1002  # flags = MAP_ANONYMOUS|MAP_PRIVATE
+  mov     r8, -1  # fd
+  mov     r9, 0  # offset
+  call    _mmap
+  pop     rsi
 
   # if mmap failed, return NULL
-  cmp rax, -1  # MAP_FAILED
-  jne 0f  # _coro_start__mmap_ok
-  xor rax, rax
+  cmp     rax, -1  # MAP_FAILED
+  jne     _coro_start__mmap_ok
+  xor     rax, rax
   ret
 
-0: _coro_start__mmap_ok:
+_coro_start__mmap_ok:
   # r9 = rsp from call to coro_start; switch to coroutine stack
-  lea r9, [rax + rsi]
-  xchg rsp, r9
+  lea     r9, [rax + rsi]
+  xchg    rsp, r9
 
   # set up the new coroutine object by pushing the necessary stuff
-  push 0  # coro that delegated to this coro
-  push 0  # coro that this coro delegated to
-  push 0x01  # flags (not started)
-  push r15  # saved r15
-  push r14  # saved r14
-  push r13  # saved r13
-  push r12  # saved r12
-  push rbx  # saved rbx
-  push 0  # rbp for frame from call to coro_start (will be filled in later)
-  push 0  # rsp for frame from call to coro_start (will be filled in later)
-  push rsi  # stack size
-  push rax  # stack base addr
-  mov r8, rsp  # r8 = pointer to coroutine object
+  push    0  # coro that delegated to this coro
+  push    0  # coro that this coro delegated to
+  push    0x01  # flags (not started)
+  push    r15  # saved r15
+  push    r14  # saved r14
+  push    r13  # saved r13
+  push    r12  # saved r12
+  push    rbx  # saved rbx
+  push    0  # rbp for frame from call to coro_start (will be filled in later)
+  push    0  # rsp for frame from call to coro_start (will be filled in later)
+  push    rsi  # stack size
+  push    rax  # stack base addr
+  mov     r8, rsp  # r8 = pointer to coroutine object
 
   # set up the caller's stack. it will have a fake return address on it that
   # will make the coroutine function return to coro_return when it returns
-  lea rdx, [rip + _coro_return]
-  push rdx
+  lea     rdx, [rip + _coro_return]
+  push    rdx
 
   # copy the caller's stack downward until src and (new) rsp match
-  mov rcx, rbp
-1: _coro_start__copy_stack_frame:
-  push [rcx]
-  sub rcx, 8
-  cmp rcx, r9
-  jge 1b # _coro_start__copy_stack_frame
+  mov     rcx, rbp
+_coro_start__copy_stack_frame:
+  push    [rcx]
+  sub     rcx, 8
+  cmp     rcx, r9
+  jge     _coro_start__copy_stack_frame
 
   # fill in the rsp and rbp for the coroutine object. correct rbp by the
   # difference between the new stack and old stack
-  lea r10, [rsp + rbp]  # r10 = old rbp + new stack (rsp) - old stack (r9)
-  sub r10, r9
-  mov [r8 + 0x10], rsp
-  mov [r8 + 0x18], r10
+  lea     r10, [rsp + rbp]  # r10 = old rbp + new stack (rsp) - old stack (r9)
+  sub     r10, r9
+  mov     [r8 + 0x10], rsp
+  mov     [r8 + 0x18], r10
 
   # switch back to main stack and remove the last stack frame - we just moved it
   # into the coroutine object
-  mov rsp, rbp
-  pop rbp
+  mov     rsp, rbp
+  pop     rbp
 
   # return the coroutine object instead, skipping that frame entirely
-  mov rax, r8
+  mov     rax, r8
   ret
 
 
 
 _coro_return:
   # rdi = coroutine, rsi = 0 (for return value from coro_continue)
-  mov rdi, rsp
-  xor rsi, rsi
+  mov     rdi, rsp
+  xor     rsi, rsi
 
   # later on, we'll mark the coroutine as finished by setting saved_rsp to NULL,
   # and save the return value in the saved_rbp field. the xchg opcodes below
   # will do this appropriately
-  xor rsp, rsp
-  mov rbp, rax
+  xor     rsp, rsp
+  mov     rbp, rax
 
   # check if it's a delegate; if it is, then we should return to from_coro
   # instead of swapping the contexts now
-  mov rcx, [rdi + 0x58]
-  test rcx, rcx
-  jz 3f  # _coro_return__not_delegate
+  mov     rcx, [rdi + 0x58]
+  test    rcx, rcx
+  jz      _coro_return__not_delegate
 
 _coro_return__is_delegate:
   # if the coro is a delegate, return to from_coro instead. this means moving
@@ -121,62 +121,62 @@ _coro_return__is_delegate:
   # this, we load the context from the terminated coro, then immediately call
   # coro_continue on from_coro (so the just-restored context is saved there
   # instead). from_coro resumes from the call to coro_delegate.
-  xchg [rdi + 0x10], rsp
-  xchg [rdi + 0x18], rbp
-  xchg [rdi + 0x20], rbx
-  xchg [rdi + 0x28], r12
-  xchg [rdi + 0x30], r13
-  xchg [rdi + 0x38], r14
-  xchg [rdi + 0x40], r15
+  xchg    [rdi + 0x10], rsp
+  xchg    [rdi + 0x18], rbp
+  xchg    [rdi + 0x20], rbx
+  xchg    [rdi + 0x28], r12
+  xchg    [rdi + 0x30], r13
+  xchg    [rdi + 0x38], r14
+  xchg    [rdi + 0x40], r15
 
   # unlink the delegate and resume from_coro
-  xor rdx, rdx
-  mov [rdi + 0x58], rdx
-  mov [rcx + 0x50], rdx
-  mov rdi, rcx
+  xor     rdx, rdx
+  mov     [rdi + 0x58], rdx
+  mov     [rcx + 0x50], rdx
+  mov     rdi, rcx
 
   # the return value is the function's return value (which is still in rax)
-  mov rsi, rax
+  mov     rsi, rax
 
-3: _coro_return__not_delegate:
+_coro_return__not_delegate:
 
 .globl _coro_continue
 _coro_continue:
   # if the coro has a delegate, continue that one instead
-  mov rdx, [rdi + 0x50]
-  test rdx, rdx
-  cmovnz rdi, rdx
-  jnz 3b  # _coro_continue
+  mov     rdx, [rdi + 0x50]
+  test    rdx, rdx
+  cmovnz  rdi, rdx
+  jnz     _coro_continue
 
   # if saved_rsp is blank, the coroutine is finished
-  xchg [rdi + 0x10], rsp
-  test rsp, rsp
-  jnz 4f  # _coro_continue__coroutine_can_continue
+  xchg    [rdi + 0x10], rsp
+  test    rsp, rsp
+  jnz     _coro_continue__coroutine_can_continue
 
   # return to the caller's stack and return NULL
-  xchg [rdi + 0x10], rsp
-  xor rax, rax
+  xchg    [rdi + 0x10], rsp
+  xor     rax, rax
   ret
 
-4: _coro_continue__coroutine_can_continue:
+_coro_continue__coroutine_can_continue:
   # swap the contexts and return the second argument
-  xchg [rdi + 0x18], rbp
-  xchg [rdi + 0x20], rbx
-  xchg [rdi + 0x28], r12
-  xchg [rdi + 0x30], r13
-  xchg [rdi + 0x38], r14
-  xchg [rdi + 0x40], r15
+  xchg    [rdi + 0x18], rbp
+  xchg    [rdi + 0x20], rbx
+  xchg    [rdi + 0x28], r12
+  xchg    [rdi + 0x30], r13
+  xchg    [rdi + 0x38], r14
+  xchg    [rdi + 0x40], r15
 
   # mark the coroutine as started. if it wasn't started before, return the
   # pointer to the coroutine instead of the passed-in value
-  xor rax, rax
-  xchg [rdi + 0x48], rax
-  test rax, rax
-  cmovnz rsi, rdi
+  xor     rax, rax
+  xchg    [rdi + 0x48], rax
+  test    rax, rax
+  cmovnz  rsi, rdi
 
   # return. we don't have to do anything special here; the correct return
   # address should be next on the stack that we just swapped to
-  mov rax, rsi
+  mov     rax, rsi
   ret
 
 
@@ -207,19 +207,19 @@ _coro_delegate:
   # - from_coro state: location/context in from_coro
 
   # set the delegated to/from pointers appropriately
-  mov [rdi + 0x50], rsi  # from_coro->delegated_to_coro = to_coro
-  mov [rsi + 0x58], rdi  # to_coro->delegated_from_coro = from_coro
+  mov     [rdi + 0x50], rsi  # from_coro->delegated_to_coro = to_coro
+  mov     [rsi + 0x58], rdi  # to_coro->delegated_from_coro = from_coro
 
   # get the context from from_coro (this is where to_coro should return to when
   # it yields) and save the current context to it (this is where to_coro should
   # return to when it returns, at the coro_delegate callsite)
-  xchg [rdi + 0x10], rsp
-  xchg [rdi + 0x18], rbp
-  xchg [rdi + 0x20], rbx
-  xchg [rdi + 0x28], r12
-  xchg [rdi + 0x30], r13
-  xchg [rdi + 0x38], r14
-  xchg [rdi + 0x40], r15
+  xchg    [rdi + 0x10], rsp
+  xchg    [rdi + 0x18], rbp
+  xchg    [rdi + 0x20], rbx
+  xchg    [rdi + 0x28], r12
+  xchg    [rdi + 0x30], r13
+  xchg    [rdi + 0x38], r14
+  xchg    [rdi + 0x40], r15
 
   # now the various locations hold:
   # - cpu regs: location/context outside coros
@@ -228,17 +228,17 @@ _coro_delegate:
   # to get to the state we want, just resume to_coro as if we had called
   # coro_continue(to_coro, NULL). this will swap the contents of the first two
   # locations
-  mov rdi, rsi
-  xor rsi, rsi
-  jmp 3b  # _coro_continue
+  mov     rdi, rsi
+  xor     rsi, rsi
+  jmp     _coro_continue
 
 
 
 .globl _coro_started
 _coro_started:
   # return !coro->not_started
-  mov rax, [rdi + 0x48]
-  xor rax, 1
+  mov     rax, [rdi + 0x48]
+  xor     rax, 1
   ret
 
 
@@ -246,10 +246,10 @@ _coro_started:
 .globl _coro_finished
 _coro_finished:
   # return 1 if there's no saved rsp (so we can't continue); else 0
-  xor rax, rax
-  mov rcx, [rdi + 0x10]
-  test rcx, rcx
-  setz al
+  xor     rax, rax
+  mov     rcx, [rdi + 0x10]
+  test    rcx, rcx
+  setz    al
   ret
 
 
@@ -258,7 +258,7 @@ _coro_finished:
 _coro_return_value:
   # if the coro is finished, the return value is stored in the rbp slot. if it's
   # not finished, the caller should already know this
-  mov rax, [rdi + 0x18]
+  mov     rax, [rdi + 0x18]
   ret
 
 
@@ -269,6 +269,6 @@ _coro_close:
   # you'll get a segfault. that's what you get for not reading the dox
 
   # just call munmap
-  mov rsi, [rdi + 0x08]  # stack size
-  mov rdi, [rdi + 0x00]  # stack base
-  jmp _munmap
+  mov     rsi, [rdi + 0x08]  # stack size
+  mov     rdi, [rdi + 0x00]  # stack base
+  jmp     _munmap
